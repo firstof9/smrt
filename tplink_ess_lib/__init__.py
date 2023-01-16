17ddb052edb478b25e7c5d3143bfb4bbf64071d6
Simon Roberts <lyricnz@users.noreply.github.com>
2023-01-17 09:49:01 +1100

    Add query method, parse results into list of key:value dict (#13)
    * Change discovery to return list[dict]
    
    * Add query() method to tplink_ess for stats etc
    
    * Improve parsing of responses. Add vlan field mapping
    
    * Include raw/unmapped fields in output

--- tplink_ess_lib/__init__.py
+++ tplink_ess_lib/__init__.py
@@ -11,6 +11,27 @@ _LOGGER = logging.getLogger(__name__)
 class tplink_ess:
     """Represent a tplink ess switch."""
 
+    RESULT_FIELD_LOOKUP = {
+        'Status': {
+            0: 'Enabled',
+            1: 'Disabled',
+        },
+        'Link Status': {
+            0: 'Link Down',
+            1: 'AUTO',
+            2: 'MH10',
+            3: 'MF10',
+            4: 'MH100',
+            5: '100Full',
+            6: '1000Full',
+        }
+    }
+
+    RESULT_TYPE_FIELDS = {
+        'stats': ('Port', 'Status', 'Link Status', 'TxGoodPkt', 'TxBadPkt', 'RxGoodPkt', 'RxBadPkt'),
+        'vlan': ('VLAN ID', 'Member Ports', 'Tagged Ports', 'VLAN Name'),
+    }
+
     def __init__(
         self, host_mac: str = "", user: str = "", pwd: str = "", switch_mac = "",
     ) -> None:
@@ -21,24 +42,33 @@ class tplink_ess:
         self._switch_mac = switch_mac
         self._data = {}
 
-    async def discovery(self) -> dict:
+    async def discovery(self) -> list[dict]:
         """Return result of auto discovery as dict."""
         if not self._host_mac:
             _LOGGER.error("MAC address missing.")
             raise MissingMac
-        net = Network(self._host_mac)
-        net.send(Protocol.DISCOVERY, {})
-        switches = {}
-        i = 0
-        while True:
-            try:
-                header, payload = net.receive()
-                payload = self.parse_payload(payload)
-                switches[i] = payload
-                i += 1
-            except ConnectionProblem:
-                break
+
+        switches = []
+        with Network(self._host_mac) as net:
+            net.send(Protocol.DISCOVERY, {})
+            while True:
+                try:
+                    header, payload = net.receive()
+                    switches.append(self.parse_response(payload))
+                except ConnectionProblem:
+                    break
         return switches
+
+    async def query(self, switch_mac: str, action: str) -> dict:
+        """send a query to a specific switch and return the results as a dict"""
+        if not self._host_mac:
+            _LOGGER.error("MAC address missing.")
+            raise MissingMac
+
+        with Network(mac=self._host_mac, switch_mac=switch_mac) as net:
+            header, payload = net.query(Protocol.GET, [(Protocol.tp_ids[action], b'')])
+            return self.parse_response(payload)
+
 
     async def update_data(self) -> dict:
         """Refresh switch data."""
@@ -53,16 +83,30 @@ class tplink_ess:
 
         for action in actions:
             header, payload = net.query(Protocol.GET, [(actions[action], b"")])
-            self._data[action] = self.parse_payload(payload)
+            self._data[action] = self.parse_response(payload)
 
         return self._data
 
-    def parse_payload(payload) -> dict:
+    def parse_response(self, payload) -> dict:
         """Parse the payload into a dict."""
+        # all payloads are list of tuple:3. if the third value is a tuple/list, it can be field-mapped
         _LOGGER.debug("Payload in: %s", payload)
         output = {}
-        for item in payload:
-            type_id, type_name, data = item
+        for type_id, type_name, data in payload:
+            if type(data) in [tuple, list]:
+                if fields := tplink_ess.RESULT_TYPE_FIELDS.get(type_name):
+                    mapped_data = {}
+                    for k, v in zip(fields, data):
+                        if mv := tplink_ess.RESULT_FIELD_LOOKUP.get(k):
+                            mapped_data[k] = mv.get(v)
+                            mapped_data[k + " Raw"] = v
+                        else:
+                            mapped_data[k] = v
+                    data = mapped_data
+                data_list = output.get(type_name, [])
+                data_list.append(data)
+                data = data_list
+
             output[type_name] = data
         _LOGGER.debug("Payload parse: %s", output)
         return output
