efc34668a7aa49201a4712b69e0e77333ef1e2af
Simon Roberts <lyricnz@gmail.com>
2023-01-16 12:40:37 +1100

    Add query() method to tplink_ess for stats etc

--- tplink_ess_lib/__init__.py
+++ tplink_ess_lib/__init__.py
@@ -11,6 +11,26 @@ _LOGGER = logging.getLogger(__name__)
 class tplink_ess:
     """Represent a tplink ess switch."""
 
+    result_field_lookup = {
+        'Status': {
+            0: 'Enabled',
+            1: 'Disabled',
+        },
+        'Link Status': {
+            0: 'Link Down',
+            1: 'AUTO',
+            2: 'MH10',
+            3: 'MF10',
+            4: 'MH100',
+            5: '100Full',
+            6: '1000Full',
+        }
+    }
+
+    result_type_fields = {
+        'stats': ('Port', 'Status', 'Link Status', 'TxGoodPkt', 'TxBadPkt', 'RxGoodPkt', 'RxBadPkt')
+    }
+
     def __init__(
         self, host_mac: str = "", user: str = "", pwd: str = "", switch_mac = "",
     ) -> None:
@@ -38,6 +58,17 @@ class tplink_ess:
                     break
         return switches
 
+    async def query(self, switch_mac: str, action: str) -> dict:
+        """send a query to a specific switch and return the results as a dict"""
+        if not self._host_mac:
+            _LOGGER.error("MAC address missing.")
+            raise MissingMac
+
+        with Network(mac=self._host_mac, switch_mac=switch_mac) as net:
+            header, payload = net.query(Protocol.GET, [(Protocol.tp_ids[action], b'')])
+            return self.parse_query_response(payload)
+
+
     async def update_data(self) -> dict:
         """Refresh switch data."""
         try:
@@ -63,4 +94,13 @@ class tplink_ess:
             type_id, type_name, data = item
             output[type_name] = data
         _LOGGER.debug("Payload parse: %s", output)
+        return output
+
+    def parse_query_response(self, payload) -> list[dict]:
+        """Parse a list of records of the same type into a list of dicts"""
+        output = []
+        for type_id, type_name, data in payload:
+            if fields := self.result_type_fields.get(type_name):
+                data = {k: self.result_field_lookup.get(k, {}).get(v, v) for k, v in zip(fields, data)}
+            output.append(data)
         return output
