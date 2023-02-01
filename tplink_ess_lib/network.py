5d2341e6ee974746618f237a887c5782acd8c3d0
Simon Roberts <lyricnz@users.noreply.github.com>
2023-02-01 11:55:58 +1100

    Change messages about unexpected sequence_id or host_mac to debug-level (#31)

--- tplink_ess_lib/network.py
+++ tplink_ess_lib/network.py
@@ -107,8 +107,8 @@ class Network:
             _LOGGER.debug("Received Payload: %s", str(payload))
             # check sequence_id alignment
             if self.sequence_id != header["sequence_id"] and not testing:
-                _LOGGER.warning(
-                    "##### Ignoring sequence_id %d expected %d",
+                _LOGGER.debug(
+                    "Ignoring sequence_id %d expected %d",
                     header["sequence_id"],
                     self.sequence_id,
                 )
@@ -116,8 +116,8 @@ class Network:
             # check host_mac alignment
             data_mac = mac_to_str(header["host_mac"])
             if self.host_mac != data_mac and not testing:
-                _LOGGER.warning(
-                    "##### Ignoring host-mac %s expected %s", data_mac, self.host_mac
+                _LOGGER.debug(
+                    "Ignoring host-mac %s expected %s", data_mac, self.host_mac
                 )
                 continue
             self.token_id = header["token_id"]
